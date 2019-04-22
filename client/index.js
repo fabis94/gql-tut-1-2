86031ac0a06ce62cefe29806cb532e76cca12c7f
Fabians <fabis94@live.com>
2019-04-22 16:05:11 +0300

    Working on client

--- client/index.js
+++ client/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+
+import SongCreate from "./components/SongCreate";
+import SongList from "./components/SongList";
+import App from "./components/App";
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="/songs/new" component={SongCreate} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
